0aef641189e011144a627daa35a909c2307c36d3
sakshikharche4 <sakshi.kharche4@gmail.com>
2026-02-23 17:45:00 +0530

    added files

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Card from "../components/Card";
-import Hero from "../components/Hero.Jsx";
 
 const router = createBrowserRouter([
   {
